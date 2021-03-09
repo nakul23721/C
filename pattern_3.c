acbecf9ee20a7a03411793f5ea28663be8f2f9b7
Nakul Verma <nakulverma2312@gmail.com>
2021-03-09 19:26:03 +0530

    Pattern 3

--- pattern_3.c
+++ pattern_3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main() {
+void main() 
+{
    int i, space, rows, k = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
@@ -13,5 +14,4 @@ int main() {
       }
       printf("\n");
    }
-   return 0;
 }
