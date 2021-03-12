8a8abddc6c4a367b34fa15c3d519abd675be6754
Nakul Verma <nakulverma2312@gmail.com>
2021-03-12 22:12:36 +0530

    Printing Multiplication Table Program

--- Table.c
+++ Table.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main() {
+void main() {
     int n, i;
     printf("Enter an integer: ");
     scanf("%d", &n);
     for (i = 1; i <= 10; ++i) {
         printf("%d * %d = %d \n", n, i, n * i);
     }
-    return 0;
 }
