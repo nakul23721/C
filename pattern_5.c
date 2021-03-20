5560bcfc96df76a2470a964a634262920c74b50f
Nakul Verma <nakulverma2312@gmail.com>
2021-03-20 22:19:16 +0530

    Pattern 5

--- pattern_5.c
+++ pattern_5.c
@@ -7,7 +7,8 @@ void main()
    for (i = 0; i < rows; i++) {
       for (space = 1; space <= rows - i; space++)
          printf("  ");
-      for (j = 0; j <= i; j++) {
+      for (j = 0; j <= i; j++) 
+      {
          if (j == 0 || i == 0)
             coef = 1;
          else
