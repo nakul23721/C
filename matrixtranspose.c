74b6579bc7a55b7326872a9ae94cb70d8e319342
Nakul Verma <nakulverma2312@gmail.com>
2021-03-23 18:54:05 +0530

    Matrix Transpose Program

--- matrixtranspose.c
+++ matrixtranspose.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main() {
+void main() 
+{
     int a[10][10], transpose[10][10], r, c, i, j;
     printf("Enter rows and columns: ");
     scanf("%d %d", &r, &c);
 
-    // Assigning elements to the matrix
     printf("\nEnter matrix elements:\n");
     for (i = 0; i < r; ++i)
         for (j = 0; j < c; ++j) {
@@ -12,7 +12,6 @@ int main() {
             scanf("%d", &a[i][j]);
         }
 
-    // Displaying the matrix a[][]
     printf("\nEntered matrix: \n");
     for (i = 0; i < r; ++i)
         for (j = 0; j < c; ++j) {
@@ -21,13 +20,12 @@ int main() {
                 printf("\n");
         }
 
-    // Finding the transpose of matrix a
+    
     for (i = 0; i < r; ++i)
         for (j = 0; j < c; ++j) {
             transpose[j][i] = a[i][j];
         }
 
-    // Displaying the transpose of matrix a
     printf("\nTranspose of the matrix:\n");
     for (i = 0; i < c; ++i)
         for (j = 0; j < r; ++j) {
@@ -35,5 +33,4 @@ int main() {
             if (j == r - 1)
                 printf("\n");
         }
-    return 0;
 }
