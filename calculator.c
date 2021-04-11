2b383188cb546b8adf8a27461d439a8cfe7386e4
Nakul Verma <nakulverma2312@gmail.com>
2021-04-11 20:10:38 +0530

    Calculator Program

--- calculator.c
+++ calculator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main() {
+void main() 
+{
     char operator;
     double first, second;
     printf("Enter an operator (+, -, *,): ");
@@ -20,10 +21,8 @@ int main() {
     case '/':
         printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
         break;
-        // operator doesn't match any case constant
     default:
         printf("Error! operator is not correct");
     }
 
-    return 0;
 }
