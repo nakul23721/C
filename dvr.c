c3809ef66c7ef6e7fa778cd25203f9dc33b072b1
Nakul Verma <nakulverma2312@gmail.com>
2021-04-27 21:11:44 +0530

    DVR Program

--- dvr.c
+++ dvr.c
@@ -4,12 +4,12 @@ struct node
     unsigned dist[20];
     unsigned from[20];
 }rt[10];
-int main()
+void main()
 {
     int costmat[20][20];
     int nodes,i,j,k,count=0;
     printf("\nEnter the number of nodes : ");
-    scanf("%d",&nodes);//Enter the nodes
+    scanf("%d",&nodes);
     printf("\nEnter the cost matrix :\n");
     for(i=0;i<nodes;i++)
     {
@@ -17,19 +17,18 @@ int main()
         {
             scanf("%d",&costmat[i][j]);
             costmat[i][i]=0;
-            rt[i].dist[j]=costmat[i][j];//initialise the distance equal to cost matrix
+            rt[i].dist[j]=costmat[i][j];
             rt[i].from[j]=j;
         }
     }
         do
         {
             count=0;
-            for(i=0;i<nodes;i++)//We choose arbitary vertex k and we calculate the direct distance from the node i to k using the cost matrix
-            //and add the distance from k to node j
+            for(i=0;i<nodes;i++)
             for(j=0;j<nodes;j++)
             for(k=0;k<nodes;k++)
                 if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
-                {//We calculate the minimum distance
+                {
                     rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
                     rt[i].from[j]=k;
                     count++;
