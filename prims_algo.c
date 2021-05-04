a33adaec3eea1cca75cdf65fb967c8f81fad6716
Nakul Verma <nakulverma2312@gmail.com>
2021-05-04 20:23:33 +0530

    Prims algo

--- prims_algo.c
+++ prims_algo.c
@@ -1,12 +1,10 @@
 #include <bits/stdc++.h> 
 using namespace std; 
-  
-// Number of vertices in the graph  
+ 
 #define V 5  
  
 int minKey(int key[], bool mstSet[])  
-{  
-    // Initialize min value  
+{   
     int min = INT_MAX, min_index;  
   
     for (int v = 0; v < V; v++)  
@@ -34,9 +32,8 @@ void primMST(int graph[V][V])
         key[i] = INT_MAX, mstSet[i] = false;  
   
     key[0] = 0;  
-    parent[0] = -1; // First node is always root of MST  
+    parent[0] = -1;  
   
-    // The MST will have V vertices 
     for (int count = 0; count < V - 1; count++) 
     {  
         
@@ -48,21 +45,20 @@ void primMST(int graph[V][V])
             if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])  
                 parent[v] = u, key[v] = graph[u][v];  
     }  
-  
-    // print the constructed MST  
+   
     printMST(parent, graph);  
 }  
   
-// Driver code 
+
 int main()  
 {  
     int graph[V][V] = { { 0, 2, 0, 6, 0 },  
                         { 2, 0, 3, 8, 5 },  
                         { 0, 3, 0, 0, 7 },  
-                        { 6, 8, 0, 0, 9 },  
-                        { 0, 5, 7, 9, 0 } };  
+                        { 6, 5, 7, 0, 9 },  
+                        { 0, 8, 0, 9, 0 },
+                      };  
   
-    // Print the solution  
     primMST(graph);  
   
     return 0;  
