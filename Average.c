b3d849958606e061ed47f81720359471097b264b
Nakul Verma <nakulverma2312@gmail.com>
2021-06-06 22:11:50 +0530

    Average Program

--- Average.c
+++ Average.c
@@ -8,7 +8,7 @@ void main()
     scanf("%d", &n);
 
     while (n > 100 || n < 1) {
-        printf("Error! number should in range of (1 to 100).\n");
+        printf("Error!!! number should in range of (1 to 100).\n");
         printf("Enter the number again: ");
         scanf("%d", &n);
     }
