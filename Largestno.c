be5f8df0783cbc72ddd2b364c3bdeb46acb064e7
Nakul Verma <nakulverma2312@gmail.com>
2021-06-12 23:53:50 +0530

    Largest number program

--- Largestno.c
+++ Largestno.c
@@ -9,14 +9,14 @@ void main()
 
     if (n1 >= n2 && n1 >= n3)
         
-            printf("%.2f is the largest number.", n1);
+            printf("%.2f is largest number.", n1);
 
     if (n2 >= n1 && n2 >= n3)
         
-            printf("%.2f is the largest number.", n2);
+            printf("%.2f is largest number.", n2);
 
     if (n3 >= n1 && n3 >= n2)
         
-            printf("%.2f is the largest number.", n3);
+            printf("%.2f is largest number.", n3);
 
 }
